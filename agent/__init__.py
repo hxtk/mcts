d5411e7d907d44bf74f8bb98c74ded5784fa374b
Peter <peter@psanders.me>
2023-03-12 14:15:15 -0400

    split out training module

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,9 +1,10 @@
 from agent import _agent
 from agent import _model
+from agent import _train
 
 residual_model = _model.residual_model
-train = _model.train
-training_batch = _model.training_batch
+train = _train.train
+training_batch = _train.training_batch
 
 CompetitivePlayer = _agent.CompetitivePlayer
 TrainingPlayer = _agent.TrainingPlayer
